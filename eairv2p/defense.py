598e13fdfdbebd52c0b4302a50ab42eefd6a8d4c
sergiu12 <30621746+sergiu12@users.noreply.github.com>
2017-10-08 14:56:35 +0300

    defense which is a slight modification of the sample_defense ens_adv_inception_resnet_v2 from github cleverhans

---
+++ eairv2p/defense.py
@@ -0,0 +1,135 @@
+"""Implementation of sample defense.
+
+This defense loads inception resnet v2 checkpoint and classifies all images
+using loaded checkpoint.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import numpy as np
+from scipy.misc import imread
+import scipy.ndimage
+import scipy.stats
+
+import tensorflow as tf
+
+import inception_resnet_v2
+
+DUPS_NUM = 3 # number of transformed images when including no operation at all
+
+slim = tf.contrib.slim
+
+
+tf.flags.DEFINE_string(
+    'master', '', 'The address of the TensorFlow master to use.')
+
+tf.flags.DEFINE_string(
+    'checkpoint_path', '', 'Path to checkpoint for inception network.')
+
+tf.flags.DEFINE_string(
+    'input_dir', '', 'Input directory with images.')
+
+tf.flags.DEFINE_string(
+    'output_file', '', 'Output file to save labels.')
+
+tf.flags.DEFINE_integer(
+    'image_width', 299, 'Width of each input images.')
+
+tf.flags.DEFINE_integer(
+    'image_height', 299, 'Height of each input images.')
+
+tf.flags.DEFINE_integer(
+    'batch_size', 16*3, 'How many images process at one time.')
+
+FLAGS = tf.flags.FLAGS
+
+
+def load_images(input_dir, batch_shape):
+  """Read png images from input directory in batches.
+
+  Args:
+    input_dir: input directory
+    batch_shape: shape of minibatch array, i.e. [batch_size, height, width, 3]
+
+  Yields:
+    filenames: list file names without path of each image
+      Lenght of this list could be less than batch_size, in this case only
+      first few images of the result are elements of the minibatch.
+    images: array with all images from this batch
+  """
+  images = np.zeros(batch_shape)
+  filenames = []
+  idx = 0
+  batch_size = batch_shape[0]
+  alpha = 5
+  for filepath in tf.gfile.Glob(os.path.join(input_dir, '*.png')):
+    with tf.gfile.Open(filepath) as f:
+      image = imread(f, mode='RGB').astype(np.float) / 255.0
+      image_dups = np.zeros((batch_shape[1], batch_shape[2], batch_shape[3], DUPS_NUM))
+      image_dups[:, :, :, 0] = image
+      for dim in range(3):
+          image_blurred = scipy.ndimage.gaussian_filter(image[:, :, dim], 1)  # blurred
+          image_dups[:, :, dim, 1] = image_blurred
+          image_dups[:, :, dim, 2] = image[:, :, dim] + alpha * (image[:, :, dim] - image_blurred)  # sharpened
+    # Images for inception classifier are normalized to be in [-1, 1] interval.
+    for i_dup in range(DUPS_NUM):
+        images[idx, :, :, :] = image_dups[:, :, :, i_dup] * 2.0 - 1.0
+        idx += 1
+    filenames.append(os.path.basename(filepath))
+    if idx == batch_size:
+      yield filenames, images
+      filenames = []
+      images = np.zeros(batch_shape)
+      idx = 0
+  if idx > 0:
+    yield filenames, images
+
+
+def main(_):
+  batch_shape = [FLAGS.batch_size, FLAGS.image_height, FLAGS.image_width, 3]
+  num_classes = 1001
+
+  tf.logging.set_verbosity(tf.logging.INFO)
+
+  with tf.Graph().as_default():
+    # Prepare graph
+    x_input = tf.placeholder(tf.float32, shape=batch_shape)
+
+    with slim.arg_scope(inception_resnet_v2.inception_resnet_v2_arg_scope()):
+      _, end_points = inception_resnet_v2.inception_resnet_v2(
+          x_input, num_classes=num_classes, is_training=False)
+
+    predicted_labels = tf.argmax(end_points['Predictions'], 1)
+
+    # Run computation
+    saver = tf.train.Saver(slim.get_model_variables())
+    session_creator = tf.train.ChiefSessionCreator(
+        scaffold=tf.train.Scaffold(saver=saver),
+        checkpoint_filename_with_path=FLAGS.checkpoint_path,
+        master=FLAGS.master)
+
+    with tf.train.MonitoredSession(session_creator=session_creator) as sess:
+      with tf.gfile.Open(FLAGS.output_file, 'w') as out_file:
+        for filenames, images in load_images(FLAGS.input_dir, batch_shape):
+          labels = sess.run(predicted_labels, feed_dict={x_input: images})
+          # print("Filenames length: %d" % len(filenames))
+          # print("labels length: %d" % len(labels))
+          tlabels = []
+          for idups in range(0, len(labels), DUPS_NUM):
+                labels_of_dups = labels[idups:idups+DUPS_NUM]
+                dups_mode = scipy.stats.mode(labels_of_dups)
+                if dups_mode[1] > 1:
+                    tlabels.append(int(dups_mode[0]))
+                else:
+                    tlabels.append(labels_of_dups[0])
+          # print("labels length after dups squeeze: %d" % len(tlabels))
+          for filename, label in zip(filenames, tlabels):
+            out_file.write('{0},{1}\n'.format(filename, label))
+
+
+if __name__ == '__main__':
+  tf.app.run()
